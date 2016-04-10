e92da99d180c748397e9244d24ae4c6786460fab
Shadab Zafar <dufferzafar0@gmail.com>
2016-04-10 23:07:04 +0530

    Spatial Fixes

--- tests/test_plugin_manager.py
+++ tests/test_plugin_manager.py
@@ -1,10 +1,7 @@
 import unittest
 from textwrap import dedent
 
-from http_request_translator import (
-    plugin_manager as pmgr,
-    script
-)
+from http_request_translator import plugin_manager, script
 
 
 class TestPluginManager(unittest.TestCase):
@@ -13,18 +10,17 @@ class TestPluginManager(unittest.TestCase):
     # plugin_manager.get_script_class
     ###
     def test_get_script_class(self):
-        self.assertEqual(pmgr.get_script_class("ruby"), script.RubyScript)
-        self.assertRaises(ValueError, pmgr.get_script_class, "lua")
+        self.assertEqual(plugin_manager.get_script_class("ruby"), script.RubyScript)
+        self.assertRaises(ValueError, plugin_manager.get_script_class, "lua")
 
     ###
     # plugin_manager.generate_script
     ###
     def test_generate_script(self):
-        script = pmgr.generate_script(
+        script = plugin_manager.generate_script(
             "bash",
             headers=["host:github.com"],
-            details=dict(path="file.txt", method="GET")
-        )
+            details=dict(path="file.txt", method="GET"))
         result = dedent("""
             #!/usr/bin/env bash
             curl -v --request GET http://file.txt  --header "host:github.com"  --include
